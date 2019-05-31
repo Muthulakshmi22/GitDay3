74c2612ef218aef76ab63434d1e7c688ab864206
Muthulakshmi22 <muthulakshmi221997@gmail.com>
2019-05-31 13:16:14 -0700

    add class

---
+++ src/org/camp/Sampleeeee.java
@@ -0,0 +1,16 @@
+package org.camp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sampleeeee {
+	public static void main(String[] args) {
+List li=new ArrayList<>();
+li.add(10);
+li.add(20);
+li.add(30);
+System.out.println(li);
+
+	}
+
+}
